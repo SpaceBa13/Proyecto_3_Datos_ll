797e3c5b4f7ad3b6a7d6e402ed0350ec49b72e73
genecmmm <129640770+genecmmm@users.noreply.github.com>
2024-06-13 23:44:10 -0600

    api

--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 #include <string>
 #include "guit.h"
+#include <cpprest/http_client.h>
+#include <cpprest/filestream.h>
 
+using namespace utility;           // Common utilities like string conversions
+using namespace web;               // Common features like URIs.
+using namespace web::http;         // Common HTTP functionality
+using namespace web::http::client; // HTTP client features
 using namespace std;
 
 // Función para eliminar espacios en blanco al inicio y al final de una cadena
