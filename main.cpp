bdcb20ff9e3f575bde6ee7a5b6505bf4bdb13529
Brayan Alpizar <130131491+SpaceBa13@users.noreply.github.com>
2024-06-14 04:50:56 -0600

    bugfixes

--- main.cpp
+++ main.cpp
@@ -57,6 +57,17 @@ void printUsage() {
     cout << "Enter command (type 'exit' to quit): ";
 }
 
+
+json::value build_change(string id_commit, string filename, string filename_difference) {
+    // Obtener la fecha y hora actual formateada
+    string current_datetime = get_current_datetime();
+    json::value change;
+    change[U("id_commit")] = json::value::string(to_string_t(id_commit));
+    change[U("filename")] = json::value::string(to_string_t(filename));
+    change[U("filename_difference")] = json::value::string(to_string_t(filename_difference));
+    change[U("datetime")] = json::value::string(to_string_t(current_datetime));
+    return change;
+}
    
 
 int main(int argc, char* argv[]) {
@@ -93,19 +104,11 @@ int main(int argc, char* argv[]) {
     json::value changes_array;
 
     // Primer cambio
-    json::value change1;
-    change1[U("id_commit")] = json::value::string(to_string_t("commit123"));
-    change1[U("filename")] = json::value::string(to_string_t("archivo1.txt"));
-    change1[U("filename_difference")] = json::value::string(to_string_t("archivo1_v2.txt"));
-    change1[U("datetime")] = json::value::string(to_string_t(current_datetime));
+    json::value change1 = build_change("commit123", "archivo1.txt", "archivo1_v2.txt");
     changes_array[0] = change1;
 
     // Segundo cambio
-    json::value change2;
-    change2[U("id_commit")] = json::value::string(to_string_t("commit123"));
-    change2[U("filename")] = json::value::string(to_string_t("archivo2.png"));
-    change2[U("filename_difference")] = json::value::string(to_string_t("archivo2_v2.png"));
-    change2[U("datetime")] = json::value::string(to_string_t(current_datetime));
+    json::value change2 = build_change("commit123", "archivo2.png", "archivo2_v2.png");
     changes_array[1] = change2;
 
     // Agregar el array de cambios al objeto principal
@@ -133,25 +136,16 @@ int main(int argc, char* argv[]) {
 
      // Construir el JSON según la estructura deseada
     json::value status_commit;
-    status_commit[U("id")] = json::value::string(to_string_t("c1a2b3d4e5f6g7h8i9j0"));
-    status_commit[U("id_repositorio")] = json::value::string(to_string_t("f3c3316348d5682e35cbfcf95b64fd2d"));
+    status_commit[U("id")] = json::value::string(to_string_t("af5c0553c533e1d7b77d0682e21231eb"));
+    status_commit[U("id_repositorio")] = json::value::string(to_string_t("2ab1fc6a345199263f7d77315fcb5b63"));
     status_commit[U("message")] = json::value::string(to_string_t("Initial commit"));
     status_commit[U("autor")] = json::value::string(to_string_t("John Doe"));
     status_commit[U("datetime")] = json::value::string(to_string_t(new_current_datetime));
 
     // Construir el array 'changes'
     json::value status_changes;
-    json::value change3, change4;
-
-    change3[U("id_commit")] = json::value::string(to_string_t("c1a2b3d4e5f6g7h8i9j0"));
-    change3[U("filename")] = json::value::string(to_string_t("archivo1.txt"));
-    change3[U("filename_difference")] = json::value::string(to_string_t("archivo1_v2.txt"));
-    change3[U("datetime")] = json::value::string(to_string_t(new_current_datetime));
-
-    change4[U("id_commit")] = json::value::string(to_string_t("c1a2b3d4e5f6g7h8i9j0"));
-    change4[U("filename")] = json::value::string(to_string_t("archivo2.png"));
-    change4[U("filename_difference")] = json::value::string(to_string_t("archivo2_v2.png"));
-    change4[U("datetime")] = json::value::string(to_string_t(new_current_datetime));
+    json::value change3 = build_change("af5c0553c533e1d7b77d0682e21231eb", "archivo3.txt", "archivo1_v2.txt");
+    json::value change4 = build_change("af5c0553c533e1d7b77d0682e21231eb", "archivo4.png", "archivo2_v2.png");
 
     status_changes[0] = change3;
     status_changes[1] = change4;
