7ad38bda2e3740284a9e81abd704d4ef9e60596e
Brayan Alpizar <130131491+SpaceBa13@users.noreply.github.com>
2024-06-14 01:40:39 -0600

    Conexion del init con BD

--- guit.cpp
+++ guit.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <vector>
 
+
 std::vector<std::string> stagedFiles; // Almacenar archivos pendientes de los commits
 int commitCounter = 0; 
 
@@ -14,38 +15,38 @@ void help() {
 
     std::cout << "                      Inicializar un nuevo repositorio con el nombree\n";
     std::cout << "                      Instancia un nuevo repositorio en el servidor y lo identifica con el nombre indicado por <name>.\n";
-    std::cout << "  init                Además, crea cualquier estructura de datos necesaria para llevar el control del lado del cliente\n";
-    std::cout << "                      sobre cuáles archivos están siendo controlados por el server y cuáles no. Debe crear un archivo\n";
-    std::cout << "                      llamado .guitignore que permite especificar cuáles archivos son ignorados por el control de \n";
-    std::cout << "                      versiones. El servidor almacena esta información también.\n\n";
+    std::cout << "  init                Ademï¿½s, crea cualquier estructura de datos necesaria para llevar el control del lado del cliente\n";
+    std::cout << "                      sobre cuï¿½les archivos estï¿½n siendo controlados por el server y cuï¿½les no. Debe crear un archivo\n";
+    std::cout << "                      llamado .guitignore que permite especificar cuï¿½les archivos son ignorados por el control de \n";
+    std::cout << "                      versiones. El servidor almacena esta informaciï¿½n tambiï¿½n.\n\n";
 
     std::cout << "  help                Mostrar este mensaje de ayuda\n";
-    std::cout << "                      Este comando va a mostrar en la consola la información de lo que hace cada comando en guit.\n\n";
+    std::cout << "                      Este comando va a mostrar en la consola la informaciï¿½n de lo que hace cada comando en guit.\n\n";
 
     std::cout << "                      Agregar archivos al repositorio\n";
-    std::cout << "                      Permite agregar todos los archivos que no estén registrados o que tengan nuevos cambios al repo-\n";
-    std::cout << "  add                 sitorio. Ignora los archivos que estén configurados en .guitignore. El usuario puede indicar cada\n";
+    std::cout << "                      Permite agregar todos los archivos que no estï¿½n registrados o que tengan nuevos cambios al repo-\n";
+    std::cout << "  add                 sitorio. Ignora los archivos que estï¿½n configurados en .guitignore. El usuario puede indicar cada\n";
     std::cout << "                      archivo por agregar, o puede usar el flag -A para agregar todos los archivos relevantes. Cuando\n";
     std::cout << "                      los archivos se agregan, se marcan como pendientes de commit.\n\n";
 
     std::cout << "                      Confirmar cambios con un mensaje\n";
-    std::cout << "                      Envía los archivos agregados y pendientes de commit al server. Se debe especificar un mensaje a\n";
+    std::cout << "                      Envï¿½a los archivos agregados y pendientes de commit al server. Se debe especificar un mensaje a\n";
     std::cout << "  commit <message>    la hora de hacer el commit. El server recibe los cambios, y cuando ha terminado de procesar los\n";
     std::cout << "                      cambios, retorna un id de commit al cliente generado con MD5. El server debe verificar que el \n";
-    std::cout << "                      commit del cliente esté al día con el resto de cambios realizados por otros usuarios. En caso \n";
-    std::cout << "                      de que no sea así, rechaza el commit.\n\n";
+    std::cout << "                      commit del cliente estï¿½ al dï¿½a con el resto de cambios realizados por otros usuarios. En caso \n";
+    std::cout << "                      de que no sea asï¿½, rechaza el commit.\n\n";
 
     std::cout << "                      Mostrar el estado de los archivos\n";
-    std::cout << "  status <file>       Este comando nos va a mostrar cuáles archivos han sido cambiados, agregados o eliminados de \n";
+    std::cout << "  status <file>       Este comando nos va a mostrar cuï¿½les archivos han sido cambiados, agregados o eliminados de \n";
     std::cout << "                      acuerdo al commit anterior. Si el usuario especifica <file>, muestra el historial de cambios, \n";
     std::cout << "                      recuperando el historial de cambios desde el server.\n\n";
 
-    std::cout << "                      Revertir un archivo a una confirmación específica\n";
-    std::cout << "  rollback            Permite regresar un archivo en el tiempo a un commit específico. Para esto, se comunica al server\n";
-    std::cout << "                      y recupera el archivo hasta dicha versión.\n\n";
+    std::cout << "                      Revertir un archivo a una confirmaciï¿½n especï¿½fica\n";
+    std::cout << "  rollback            Permite regresar un archivo en el tiempo a un commit especï¿½fico. Para esto, se comunica al server\n";
+    std::cout << "                      y recupera el archivo hasta dicha versiï¿½n.\n\n";
 
-    std::cout << "  reset <file>        Restablecer los cambios de un archivo a la última confirmación\n";
-    std::cout << "                      Deshace cambios locales para un archivo y lo regresa al último commit.\n\n";
+    std::cout << "  reset <file>        Restablecer los cambios de un archivo a la ï¿½ltima confirmaciï¿½n\n";
+    std::cout << "                      Deshace cambios locales para un archivo y lo regresa al ï¿½ltimo commit.\n\n";
 
     std::cout << "                      Sincronizar un archivo con el servidor\n";
     std::cout << "  sync <file>         Recupera los cambios para un archivo en el server y lo sincroniza con el archivo en el cliente.\n";
@@ -60,7 +61,7 @@ void initRepo(const std::string& name) {
 void addFiles(const std::string& options, const std::string& name) {
     if (options == "-A") {
         std::cout << "Todos los archivos agregados.\n";
-        // Simulación
+        // Simulaciï¿½n
         stagedFiles.push_back("archprue1.txt");
         stagedFiles.push_back("archprue2.txt");
     }
@@ -83,7 +84,7 @@ void commitChanges(const std::string& message) {
     std::cout << "Comenzando commit...\n";
     std::cout << "Mensaje del commit: " << message << "\n";
 
-    // Generar un ID de commit único basado en el contador
+    // Generar un ID de commit ï¿½nico basado en el contador
     std::string commitId = generarCommitId();
     std::cout << "Commit ID: " << commitId << "\n";
 
@@ -113,9 +114,4 @@ void resetFile(const std::string& file) {
 
 void syncFile(const std::string& file) {
    
-}
-
-int main() {
-    help(); // Mostrar mensaje de ayuda al inicio
-    return 0;
 }
--- main.cpp
+++ main.cpp
@@ -1,8 +1,95 @@
 #include <iostream>
 #include <string>
 #include "guit.h"
+#include <cpprest/http_client.h>
+#include <cpprest/filestream.h>
+
+using namespace std;
+using namespace utility;           // Common utilities like string conversions
+using namespace web;               // Common features like URIs.
+using namespace web::http;         // Common HTTP functionality
+using namespace web::http::client; // HTTP client features
+
+
+// Función para manejar la respuesta HTTP
+void get_id_repo(http_response response, string& ID_Repo) {
+    if (response.status_code() == status_codes::OK) {
+        wcout << U("POST exitoso. Código de estado: ") << response.status_code() << endl;
+        auto json_task = response.extract_json();
+        try {
+            json::value v = json_task.get();
+            // Extraer el valor de la etiqueta "ID_Repo" y almacenarlo en la variable ID_Repo
+            if (v.has_field(U("id"))) {
+                ID_Repo = utility::conversions::to_utf8string(v.at(U("id")).as_string());
+                wcout << U("Repositorio iniciado con existo, su id es: ") << v.at(U("id")).as_string() << endl;
+            }
+            else {
+                wcout << U("La respuesta no contiene la etiqueta 'ID_Repo'.") << endl;
+            }
+        }
+        catch (const http_exception& e) {
+            std::wcout << U("Error al obtener la respuesta JSON: ") << e.what() << endl;
+        }
+    }
+    else {
+        std::wcout << U("Error en la solicitud POST. Código de estado: ") << response.status_code() << endl;
+    }
+}
+
+void init_repo_in_server(string &id_repo, string repo_name, string description) {
+    // Crear el cliente HTTP
+    http_client client(U("https://localhost:7092"));
+
+    // Construir la URI y comenzar la solicitud
+    uri_builder builder(U("/InitRepo/"));
+
+    // Crear el objeto JSON a enviar
+    json::value init_repo_model;
+    init_repo_model[U("repo_name")] = json::value::string(utility::conversions::to_string_t(repo_name));
+    init_repo_model[U("description")] = json::value::string(utility::conversions::to_string_t(description));
+
+    // Hacer la solicitud POST
+    pplx::task<void> requestTask = client.request(methods::POST, builder.to_string(), init_repo_model.serialize(), U("application/json"))
+        .then([&id_repo](http_response response) { // Captura por referencia
+        get_id_repo(response, id_repo);
+            });
+
+    // Esperar a que la solicitud termine
+    try {
+        requestTask.wait();
+    }
+    catch (const std::exception& e) {
+        std::wcout << U("Error: ") << e.what() << std::endl;
+    }
+
+}
+
+
 
 int main(int argc, char* argv[]) {
+
+    string id_repo;
+    string repo_name = "Repo1";
+    string description = "Ola putos";
+
+    init_repo_in_server(id_repo, repo_name, description);
+
+    cout << id_repo << endl;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     if (argc < 2) {
         std::cerr << "No command provided.\n";
         help();
